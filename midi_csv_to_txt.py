2e8d8b470320997b1900e99471e18c9b02814cc5
Berwin Xie <berwin7996@gmail.com>
2014-06-24 22:56:11 -0500

    csv to txt changes

--- midi_csv_to_txt.py
+++ midi_csv_to_txt.py
@@ -3,10 +3,10 @@ import sys
 
 def main():
 	f_in = csv.reader(open(sys.argv[1], 'r'))
-	f_out = open('notes_' + sys.argv[1] + '.txt', 'wb') 
+	f_out = open('note_files/output.txt', 'wb') 
 	for line in f_in:
 		if ' Note_on_c' in line:
-			f_out.write(line[4] )
+			f_out.write(line[4].strip() + ' ')
 
 if __name__ == '__main__':
 	main()
